b760bc4c7a69717323bc7ac0f472192ddb9e9d81
Luan Natan <sousaluan.natan01@gmail.com>
2026-03-14 16:21:52 -0300

    Add scripts de comando

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Olá, Mundo! 🚀</h1>
+function Home() {
+  return <h1>Olá, Mundo! 🚀</h1>;
+}
+
+function Home() {
+  return <h1>Olá, Mundo! 🚀</h1>;
 }
 
 export default Home;
