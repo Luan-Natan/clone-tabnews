6e6be34547a62627cb114b7465d82d970b0083d3
Luan Natan <sousaluan.natan01@gmail.com>
2026-06-08 21:27:15 -0300

    Atualizando database.js para encerrar conexoes

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,6 @@
 import { Client } from "pg";
 
-async function queryAsync(queryObject) {
+async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -8,12 +8,19 @@ async function queryAsync(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
+
   await client.connect();
-  const result = await client.query(queryObject);
-  client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-  query: queryAsync,
+  query: query,
 };
