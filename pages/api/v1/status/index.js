6c09e8ee3e9f20d495227d9fe0984039736dadad
Luan Natan <sousaluan.natan01@gmail.com>
2026-06-08 21:28:31 -0300

    Atualizando endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,40 @@
 import database from "infra/database.js";
 
+async function getStatisticsPostgres() {
+  const postgresVersionResult = await database.query("SHOW server_version;");
+  const postgresVersion = postgresVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const oppenedConnectionsResult = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const oppenedConnections = parseInt(oppenedConnectionsResult.rows[0].count);
+
+  return {
+    postgresVersion: postgresVersion,
+    maxConnections: maxConnections,
+    oppenedConnections: oppenedConnections,
+  };
+}
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).send({ message: "Tudo certo!" });
+  const updatedAt = new Date().toISOString();
+  const pgStats = await getStatisticsPostgres();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgStats.postgresVersion,
+        max_connections: pgStats.maxConnections,
+        oppened_connections: pgStats.oppenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
