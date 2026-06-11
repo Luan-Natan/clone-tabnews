b4f36fb71b51732b3c6bfc3a8cf21ed564c910a0
Luan Natan <sousaluan.natan01@gmail.com>
2026-06-10 21:27:15 -0300

    teste supabase ssl mode pg stateless

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,21 @@
 import { Client } from "pg";
+import fs from "fs";
+import path from "path";
 
 async function query(queryObject) {
+  const certPath = path.resolve("./certs/prod-ca-2021.crt");
+  const ca = fs.readFileSync(certPath, "utf8");
+
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: {
+      ca,
+      rejectUnauthorized: true,
+    },
   });
 
   try {
@@ -15,6 +24,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
